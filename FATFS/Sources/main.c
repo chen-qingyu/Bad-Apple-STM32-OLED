e7f0549be020be4b75f2422ae1bd14d11f4fe6d2
chen-qingyu <236521827@qq.com>
2020-08-26 21:32:47 +0800

    update

--- FATFS/Sources/main.c
+++ FATFS/Sources/main.c
@@ -25,13 +25,13 @@ int main(void)
 
     W25QXX_Init();
 
-    res = f_mount(fs[1], "1:", 1); // 挂载FLASH.
+    res = f_mount(fs[1], "1:", 1); // 挂载FLASH。已配置好选项，如果FLASH没有文件系统则挂载时会先自动格式化
     printf("Mount res = %d.\r\n", res);
 
     res = f_open(&fp, "1:BadApple.bin", FA_READ); // 打开文件
     printf("Open res = %d.\r\n", res);
 
-    res = f_read(&fp, readData, 1, &br); // 读取一个字节
+    res = f_read(&fp, readData, 1, &br); // 读取一个字节（串口打印地址）
     printf("Read res = %d.\r\n", res);
 
     f_close(&fp);
